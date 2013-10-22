577d2bf29776bd300873c2eb5feec9258d51dfc3
Wellington Freire <wellfreire1@gmail.com>
2013-10-21 22:53:58 -0200

    4.13 done

--- restaurante_basico.rb
+++ restaurante_basico.rb
@@ -1,13 +1,25 @@
 #coding:utf-8
 
-class Restaurante	
-	def qualifica(nota, msg="Obrigado!")
-		puts "A nota do restaurante foi #{nota}. #{msg}"
+class Restaurante
+
+attr_accessor :nota
+
+	def initialize(nome)
+		puts "Criando um novo restaurante: #{nome}"
+		@nome = nome
 	end
+
+  def qualifica(msg="Obrigado!")
+  	puts "A nota do #{@nome} foi #{@nota}. #{msg}"
+  end
+
 end
 
-restaurante_um = Restaurante.new
-restaurante_dois = Restaurante.new
+restaurante_um = Restaurante.new("Fasano")
+restaurante_dois = Restaurante.new("Fogo de chão")
 
-restaurante_um.qualifica 10
-restaurante_dois.qualifica 1, "Ruim!"
+restaurante_um.nota = 10
+restaurante_dois.nota = 1
+
+restaurante_um.qualifica
+restaurante_dois.qualifica("Comida ruim.")
