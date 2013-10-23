d7d2cd27974a3bf1fdba353e8ce5b673fcdedc12
Wellington Freire <wellfreire1@gmail.com>
2013-10-23 19:33:45 -0200

    throw and catch

--- exceptions.rb
+++ exceptions.rb
@@ -12,22 +12,41 @@
 
 # puts "100/#{numero} é #{resultado}"
 
-class IdadeInsuficienteException < Exception
+# class IdadeInsuficienteException < Exception
+# end
+
+# def verifica_idade(idade)
+# 	unless idade > 18
+# 		raise IdadeInsuficienteException,
+# 			"Voce precisa ser maior de idade para jogar violentos!"
+# 	end
+# end
+
+# idade = gets.to_i
+
+# begin
+# 	verifica_idade idade
+# rescue IdadeInsuficienteException => e
+# 	puts "Foi lancada a exception: #{e}"
+# rescue ArgumentError => e
+# 	puts "Lancou: #{e}"
+# end
+
+def pesquisa_banco(nome)
+	if nome.length<10
+		throw :nome_invalido, "Nome invalido, digite novamente"
+	end
+
+	"cliente #{nome}"
 end
 
-def verifica_idade(idade)
-	unless idade > 18
-		raise IdadeInsuficienteException,
-			"Voce precisa ser maior de idade para jogar violentos!"
+def executa_pesquisa(nome)
+	catch :nome_invalido do
+		cliente = pesquisa_banco(nome)
+		return cliente
 	end
 end
 
-idade = gets.to_i
-
-begin
-	verifica_idade idade
-rescue IdadeInsuficienteException => e
-	puts "Foi lancada a exception: #{e}"
-rescue ArgumentError => e
-	puts "Lancou: #{e}"
-end
+puts executa_pesquisa "ana"
+	
+puts executa_pesquisa "wellington freire"
