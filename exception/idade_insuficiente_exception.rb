e012f4e5e29dda0ffad928e95c032e7397c2e1ea
Wellington Freire <wellfreire1@gmail.com>
2013-10-23 19:58:00 -0200

    extract some class to its own file

---
+++ exception/idade_insuficiente_exception.rb
@@ -0,0 +1,2 @@
+class IdadeInsuficienteException < Exception
+end
--- exceptions.rb
+++ exceptions.rb
@@ -12,41 +12,40 @@
 
 # puts "100/#{numero} é #{resultado}"
 
-# class IdadeInsuficienteException < Exception
+require File.expand_path(File.join(File.dirname(__FILE__), "exception/idade_insuficiente_exception"))
+
+def verifica_idade(idade)
+	unless idade > 18
+		raise IdadeInsuficienteException,
+			"Voce precisa ser maior de idade para jogar violentos!"
+	end
+end
+
+idade = gets.to_i
+
+begin
+	verifica_idade idade
+rescue IdadeInsuficienteException => e
+	puts "Foi lancada a exception: #{e}"
+rescue ArgumentError => e
+	puts "Lancou: #{e}"
+end
+
+# def pesquisa_banco(nome)
+# 	if nome.length<10
+# 		throw :nome_invalido, "Nome invalido, digite novamente"
+# 	end
+
+# 	"cliente #{nome}"
 # end
 
-# def verifica_idade(idade)
-# 	unless idade > 18
-# 		raise IdadeInsuficienteException,
-# 			"Voce precisa ser maior de idade para jogar violentos!"
+# def executa_pesquisa(nome)
+# 	catch :nome_invalido do
+# 		cliente = pesquisa_banco(nome)
+# 		return cliente
 # 	end
 # end
 
-# idade = gets.to_i
-
-# begin
-# 	verifica_idade idade
-# rescue IdadeInsuficienteException => e
-# 	puts "Foi lancada a exception: #{e}"
-# rescue ArgumentError => e
-# 	puts "Lancou: #{e}"
-# end
-
-def pesquisa_banco(nome)
-	if nome.length<10
-		throw :nome_invalido, "Nome invalido, digite novamente"
-	end
-
-	"cliente #{nome}"
-end
-
-def executa_pesquisa(nome)
-	catch :nome_invalido do
-		cliente = pesquisa_banco(nome)
-		return cliente
-	end
-end
-
-puts executa_pesquisa "ana"
+# puts executa_pesquisa "ana"
 	
-puts executa_pesquisa "wellington freire"
+# puts executa_pesquisa "wellington freire"
