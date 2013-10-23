cb577d8988029cb162ce53aa0f0b6f113c5e7268
Wellington Freire <wellfreire1@gmail.com>
2013-10-22 22:07:11 -0200

    4.19 and 4.21

--- listas.rb
+++ listas.rb
@@ -13,10 +13,17 @@ class Franquia
 	end
 
 	def mostra
-		for restaurante in @restaurantes
-			puts restaurante.nome
+		@restaurantes.each do |r|
+			puts r.nome
 		end
 	end
+
+	def relatorio
+		@restaurantes.each do |r|
+			yield r
+		end
+	end
+
 end
 
 class Restaurante
@@ -39,3 +46,30 @@ franquia.adiciona restaurante_um, restaurante_dois
 franquia.mostra
 
 restaurante_um.fechar_conta :valor => 50, :nota => 9, :comentario => "Gostei!"
+
+franquia.relatorio do |r|
+	puts "Restaurante cadastrado: #{r.nome}"
+end
+
+class Turma
+
+	def initialize(*nomes)
+		@nomes = []
+		for nome in nomes
+			@nomes << nome
+		end
+	end
+
+	def each
+		@nomes.each do |nome|
+			yield nome
+		end
+	end
+
+end
+
+fj91 = Turma.new("Guilherme", "Paulo", "Paniz")
+
+fj91.each do |nome|
+	puts nome
+end
