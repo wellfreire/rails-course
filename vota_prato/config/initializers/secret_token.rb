e33458f5e63198e89a2c55406dbde5735bef17d9
Wellington Freire <wellfreire1@gmail.com>
2013-10-24 21:54:09 -0200

    Creates project 'vota prato'

---
+++ vota_prato/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = '035d68addab1cfb1e630b65f9eea7bf4386d56d6fd2ae55af53a013b2be9398f821c086a28ac44fed61bed4d947d2535020baf6ea68b6818e59b3cc431be40c5'
