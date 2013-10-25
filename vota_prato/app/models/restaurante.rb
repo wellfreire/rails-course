1beea46fbe857ec347e3ae3cb24b2f8b6e4db6e5
Wellington Freire <wellfreire1@gmail.com>
2013-10-25 21:38:50 -0200

    7.15

--- vota_prato/app/models/restaurante.rb
+++ vota_prato/app/models/restaurante.rb
@@ -1,3 +1,19 @@
+#encoding: UTF-8
+
 class Restaurante < ActiveRecord::Base
   attr_accessible :nome, :endereco, :especialidade
+
+  validates_presence_of :nome, message: "deve ser preenchido"
+  validates_presence_of :endereco, message: "deve ser preenchido"
+  validates_presence_of :especialidade, message: "deve ser preenchido"
+
+  validates_uniqueness_of :nome, message: "nome já cadastrado"
+  validates_uniqueness_of :endereco, message: "endereco já cadastrado"
+
+  validate :primeira_letra_deve_ser_maiuscula
+
+  private
+  def primeira_letra_deve_ser_maiuscula
+  	errors.add(:nome, "primeira letra deve ser maiuscula") unless nome =~ /[A-Z].*/
+  end
 end
