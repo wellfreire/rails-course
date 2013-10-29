25d9a533867b950adb67188354f976470892082d
Wellington Freire <wellfreire1@gmail.com>
2013-10-29 19:55:39 -0200

    9.13

--- vota_prato/app/controllers/restaurantes_controller.rb
+++ vota_prato/app/controllers/restaurantes_controller.rb
@@ -6,4 +6,11 @@ class RestaurantesController < ApplicationController
 	def show
 		@restaurante = Restaurante.find params[:id]
 	end
+
+	def destroy
+		@restaurante = Restaurante.find params[:id]
+		@restaurante.destroy
+
+		redirect_to action: 'index'
+	end
 end
