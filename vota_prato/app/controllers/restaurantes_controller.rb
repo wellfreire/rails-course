667de5071f1344f5acffce6b421e4ca5c7f4e5e6
Wellington Freire <wellfreire1@gmail.com>
2013-10-29 19:15:24 -0200

    9.10

--- vota_prato/app/controllers/restaurantes_controller.rb
+++ vota_prato/app/controllers/restaurantes_controller.rb
@@ -2,4 +2,8 @@ class RestaurantesController < ApplicationController
 	def index
 		@restaurantes = Restaurante.order :nome
 	end
+
+	def show
+		@restaurante = Restaurante.find params[:id]
+	end
 end
